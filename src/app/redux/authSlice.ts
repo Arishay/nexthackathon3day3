f513d97f2319843755621b4977d2fb96422d0a57
Arishay <affiliatearisha@gmail.com>
2025-01-21 23:15:55 +0500

    day 4 completed

--- src/app/redux/authSlice.ts
+++ src/app/redux/authSlice.ts
@@ -20,36 +20,70 @@
 
 // export const { register, login, logout } = authSlice.actions;
 // export default authSlice.reducer;
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+// import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+// interface User {
+//   name: string;
+//   email: string;
+//   password: string;
+// }
+
+// interface AuthState {
+//   users: User[];
+//   user: User | null;
+// }
+
+// const initialState: AuthState = {
+//   users: [],
+//   user: null,
+// };
+
+// const authSlice = createSlice({
+//   name: "auth",
+//   initialState,
+//   reducers: {
+//     register(state, action: PayloadAction<User>) {
+//       state.users.push(action.payload);
+//     },
+//     login(state, action) {
+//       const { email, password } = action.payload;
+//       const user = state.users.find((user: any) => user.email === email && user.password === password);
+//       state.user = user || null;
+//     }
+//     logout(state) {
+//       state.user = null;
+//     },
+//   },
+// });
+
+// export const { register, login, logout } = authSlice.actions;
+// export default authSlice.reducer;
+
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface User {
-  name: string;
   email: string;
   password: string;
 }
 
 interface AuthState {
-  users: User[];
   user: User | null;
+  users: User[];
 }
 
 const initialState: AuthState = {
-  users: [],
   user: null,
+  users: [],
 };
 
 const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
-    register(state, action: PayloadAction<User>) {
-      state.users.push(action.payload);
-    },
     login(state, action: PayloadAction<{ email: string; password: string }>) {
       const { email, password } = action.payload;
-      const user = state.users.find(
-        (u) => u.email === email && u.password === password
-      );
+      const user = state.users.find((user) => user.email === email && user.password === password);
       state.user = user || null;
     },
     logout(state) {
@@ -58,5 +92,5 @@ const authSlice = createSlice({
   },
 });
 
-export const { register, login, logout } = authSlice.actions;
+export const { login, logout } = authSlice.actions;
 export default authSlice.reducer;
